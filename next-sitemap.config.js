2795d3e8d0b0475352a67306a8d9abf3efcc22ac
Zenon <abhishekyadav276208@gmail.com>
2025-07-30 23:41:40 +0530

    app setup done

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://abhiworks.site",
+  generateRobotsTxt: true,
+};
---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    }
+  },
+  plugins: [require("tailwindcss-animate")],
+};
